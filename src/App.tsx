ff3cff7d6868d6bc4dcd41d8bce85293aafd2ef1
FedericoCWW <federicocandow@hotmail.com>
2026-03-21 22:02:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import "./App.css";
+import { Toaster } from "./components/refine-ui/notification/toaster";
+import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
+import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
+import { dataProvider } from "./providers/data";
+import Dashboard from "./pages/Dashboard";
+import TaskList from "./pages/tasks/TaskList";
+import TaskCreate from "./pages/tasks/TaskCreate";
+import { Home, ClipboardList, List } from "lucide-react";
+import { Layout } from "./components/refine-ui/layout/layout";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RefineKbarProvider>
+        <ThemeProvider>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              notificationProvider={useNotificationProvider()}
+              routerProvider={routerProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                projectId: "4mEOk1-5lrMPO-OBSaQ7",
+              }}
+              resources={[
+                {
+                  name: "dashboard",
+                  list: "/",
+                  meta: { label: "Index", icon: <Home /> },
+                },
+                {
+                  name: "tasks",
+                  list: "/tasks",
+                  create: "/tasks/create",
+                  meta: { label: "Tasks", icon: <ClipboardList /> },
+                },
+              ]}
+            >
+              <Routes>
+                <Route
+                  element={
+                    <Layout>
+                      <Outlet />
+                    </Layout>
+                  }
+                >
+                <Route path="/" element={<Dashboard />}/>
+                  <Route path="tasks">
+                    <Route index element={<TaskList />}/>
+                    <Route path="create" element={<TaskCreate />}/>
+                  </Route>
+                </Route>
+              </Routes>
+              <Toaster />
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </ThemeProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/tasks/TaskList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div>Lista de tareas</div>
+  )
+}
+
+export default TaskList
