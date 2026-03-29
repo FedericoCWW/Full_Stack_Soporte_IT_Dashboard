d46f4ea4c7b38c4f5795570e55ab591ea49cd4e0
Federico <133919977+FedericoCWW@users.noreply.github.com>
2026-03-29 19:58:23 -0300

    Empiezo con el backend

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { NextFunction, Request, Response } from "express";
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.get("/", (req: Request, res: Response) => {
+  res.send({ message: "Hello World" });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send("Algo salió mal");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
