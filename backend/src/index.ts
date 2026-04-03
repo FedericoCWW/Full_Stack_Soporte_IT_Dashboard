8b7ea4569ecc4b07a8a5042d869c3495a795b201
FedericoCWW <federicocandow@hotmail.com>
2026-04-03 01:29:21 -0300

    primera query realizda con Postman

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,19 @@
 import express, { NextFunction, Request, Response } from "express";
+import router from './routes/tickets'
+import cors from 'cors'
+
 const app = express();
 const port = 3000;
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
+
 app.use(express.json());
+
+app.use('/api/tickets', router)
+
 app.get("/", (req: Request, res: Response) => {
   res.send({ message: "Hello World" });
 });
---
+++ backend/src/routes/tickets.ts
@@ -0,0 +1,66 @@
+import express from "express";
+import { tickets, areas } from "../db/schema/schema";
+import { ilike, or, and, sql, eq, getTableColumns, desc } from "drizzle-orm";
+import { db } from "../db";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const { search, area, page = 1, limit = 10 } = req.query;
+
+    const current_page = Math.max(1, +page);
+    const limit_per_page = Math.max(1, +limit);
+
+    const offset = (current_page - 1) * limit_per_page;
+    const filterConditions = [];
+
+    if (search) {
+      filterConditions.push(
+        or(
+          ilike(tickets.titulo, `%${search}%`),
+          ilike(tickets.codigo, `%${search}%`),
+        ),
+      );
+    }
+    if (area) {
+      filterConditions.push(eq(tickets.area_id, Number(area)));
+    }
+    const Clausewhere =
+      filterConditions.length > 0 ? and(...filterConditions) : undefined;
+    const CountResult = await db
+      .select({ count: sql<number>`count(*)` })
+      .from(tickets)
+      .leftJoin(areas, eq(tickets.area_id, areas.id))
+      .where(Clausewhere);
+
+    const TotalCount = CountResult[0]?.count ?? 0;
+
+    const TicketList = await db
+      .select({
+        ...getTableColumns(tickets),
+        area: { ...getTableColumns(areas) },
+      })
+      .from(tickets)
+      .leftJoin(areas, eq(tickets.area_id, areas.id))
+      .where(Clausewhere)
+      .orderBy(desc(tickets.CreatedAt))
+      .limit(limit_per_page)
+      .offset(offset);
+
+    res.status(200).json({
+      data: TicketList,
+      pagination: {
+        page: current_page,
+        limit: limit_per_page,
+        total: TotalCount,
+        totalPages: Math.ceil(TotalCount / limit_per_page)
+      }
+    });
+  } catch (e) {
+    console.log(`Error encontrado: ${e}`);
+    res.status(500).json({ error: "error al obtener los tickets" });
+  }
+});
+
+export default router;
